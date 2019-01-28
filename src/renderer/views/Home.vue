5ced94caa40be25de6e9b3607efb4c6f3fd57dec
Rhio Kim <rhio.kim@gmail.com>
2019-01-28 16:41:58 +0900

    feat: add macros sample data structure

---
+++ src/renderer/views/Home.vue
@@ -0,0 +1,32 @@
+<template>
+  <select v-model="selected">
+    <option v-for="(macro, index) in macros"
+      :key="index"
+      :value="index">{{ macro.name }}
+    </option>
+  </select>
+</template>
+
+<script>
+import db from '@/assets/macros.json'
+
+export default {
+  data () {
+    return {
+      ...db,
+      selected: db.macros[0]
+    }
+  },
+  mounted () {
+    console.log(db)
+  },
+  methods: {
+  },
+  watch: {
+    selected (to, from) {
+      console.log(from, to)
+    }
+  }
+}
+</script>
+
